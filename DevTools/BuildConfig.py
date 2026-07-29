aae3280c3889d260770642f6f50f4ed0e972ec52
Kevin <kevinkor6346789@gmail.com>
2026-07-29 15:14:01 +0300

    Ok , better config

--- DevTools/BuildConfig.py
+++ DevTools/BuildConfig.py
@@ -2,17 +2,24 @@
 # Troposphere Build config
 #
 
-# System Files
+# System General
 
-Include_Init_dir = True
+Include_Init_dir = False #Includes a Init configuration with pre set files... Can break some older versions
+Include_JiboPackageManager = True #Bundle Jibo package manager (Grabs from git)
+Include_JiboBinaryWrappers = True #Bundle Wrappers for the jibo binaries inside the chroot
+
+Troposphere_Splash = "%/splash_0.png" #Set the Troposphere boot splash
+Setup_Init_Entry = True #If you wanna do everything manually leave this off
+Setup_Init_Startup_Entry = True #If you want troposphere to start with the system
+
 
 # System patches
-Include_Faster_Boot_Configs = True
+Include_Faster_Boot_Configs = True #Change with patches to make startup times faster & async!
 
 # Really leave this as is pls
 
-Include_stdlib = True
-Include_TroposphereLib = True
+Include_stdlib = True #Installs standard libraries on first boot
+Include_TroposphereLib = True #Bundles Troposphere lib
 
 # Root Mout Endpoints
 Mount_Bind = [
@@ -30,6 +37,15 @@ Mount_Devices = [
         ]
 
 
+# Init.d Files to replace (Under Faster Boot Configs):
 
+Staged_Init_FBC = [
+        "S09wifi-enable",
+        "S18udev",
+        "S36sshd",
+        "rcS"
+        ]
 
+if Include_JiboBinaryWrappers:
+    Staged_Jibo_Binary_Wrappers = []
 
